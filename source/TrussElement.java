27d87804957d25044f316eddbd2ede420495220d
AlexCharidis <alexandroscharidis@gmail.com>
2017-04-03 13:19:50 -0400

    Adjust indentation

--- source/TrussElement.java
+++ source/TrussElement.java
@@ -15,48 +15,44 @@ import processing.core.PApplet;
 
 public class TrussElement {
 
-	private Node from_n;
-	private Node to_n;
+    private Node from_n;
+    private Node to_n;
 	
-	TrussElement() { /* default constructor */ }
-	TrussElement(final Node fv3, final Node tv3) {
-		from_n  = fv3;
-		to_n    = tv3;
-	}
+    TrussElement() { /* default constructor */ }
+    TrussElement(final Node fv3, final Node tv3) {
+        from_n  = fv3;
+	to_n    = tv3;
+    }
 	
-	// getters
-	public Node fromN()    {  return from_n;  }
-	public Node toN()      {  return to_n;    }
+    // getters
+    public Node fromN()    {  return from_n;  }
+    public Node toN()      {  return to_n;    }
 
-	public float length()  {
-		float xx = to_n.x()-from_n.x();
-		float yy = to_n.y()-from_n.y();
-		return ( (float)Math.sqrt(xx*xx + yy*yy) );
-	}
+    public float length()  {
+        float xx = to_n.x()-from_n.x();
+	float yy = to_n.y()-from_n.y();
+	return ( (float)Math.sqrt(xx*xx + yy*yy) );
+    }
 	
-	// setters
-	public void setFromN(final Node f) {  from_n = f;  }
-	public void setToN(final Node t)   {  to_n   = t;  }
+    // setters
+    public void setFromN(final Node f) {  from_n = f;  }
+    public void setToN(final Node t)   {  to_n   = t;  }
 	
-	// draw on screen using given Processing Applet
-	public void draw(PApplet p5, int scale) {
-		p5.stroke(160);
-		p5.strokeWeight(1);
-		p5.line(from_n.x()*scale, from_n.y()*scale, to_n.x()*scale, to_n.y()*scale);
-	}
+    // draw on screen using given Processing Applet
+    public void draw(PApplet p5, int scale) {
+        p5.stroke(160);
+	p5.strokeWeight(1);
+	p5.line(from_n.x()*scale, from_n.y()*scale, to_n.x()*scale, to_n.y()*scale);
+    }
 	
-	/**
-	 *  @return
-	 */
-	public String toString() {
-		return "{ From: " + from_n.x() + ", " + from_n.y() + "\n" +
-        		" To: " + to_n.x() + ", " + to_n.y() + " }\n";
-	}
+    public String toString() {
+	return "{ From: " + from_n.x() + ", " + from_n.y() + "\n" +
+               " To: " + to_n.x() + ", " + to_n.y() + " }\n";
+    }
 	
-	/**
-	 *  Test client and sample execution.
-	 */
-	public static void main(String[] args) {
-	}
+    /**
+     *  Test client and sample execution.
+     */
+    public static void main(String[] args) {}
 	
 }
